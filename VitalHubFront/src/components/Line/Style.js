03910144ef6a8f704443919d9424b094f11facdc
Paulo Oliveira <paulo.oliveira.phgo@gmail.com>
2024-03-20 09:00:12 -0300

    iniciado projeto em grupo

--- VitalHubFront/src/components/Line/Style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Line = styled.View`
-border: 2px solid #8C8A97;
-margin-top: 19px;
-`
--- VitalHubFront/src/components/List/List.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-import { FlatList } from "react-native";
-
-export const ListComponent = styled(FlatList)`
-    width: 100% ;
-    margin-top: 20px ;
-`
